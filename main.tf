c712aca1044b0af44c75d9eec2d1b236641abe56
dmytro_velychko3 <you@example.com>
2023-07-11 13:56:13 +0300

    fix: changed vars

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ resource "azurerm_virtual_machine_extension" "this" {
   type                 = var.extentions.type
   type_handler_version = var.extentions.type_handler_version
   settings             = var.extentions.settings
-  tags                 = var.tags
+  tags                 = var.extentions.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,12 @@
 variable "extentions" {
   type = object({
-    name                 = optional(string),
-    virtual_machine_id   = optional(string),
+    name                 = string,
+    virtual_machine_id   = string,
     publisher            = optional(string, "Microsoft.Azure.Extensions"),
     type                 = optional(string, "CustomScript"),
     type_handler_version = optional(string, "2.0"),
-    settings             = optional(string)
+    settings             = optional(string),
+    tags                 = optional(map(any))
   })
   description = "Objects to configure extentions for virtual machine"
-}
-
-variable "tags" {
-  type        = map(any)
-  description = "Resource tags"
-  default     = {}
 }
